da1ea822b03b88f5429611ffce1dc55f943a7090
ArnaudPB <arnaud.besson@gmail.com>
2021-06-30 15:04:53 +0200

    [ADD] added SAUCE collection -models -routes -controllers) & CRUD API

--- back_end/app.js
+++ back_end/app.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const sauceRoutes = require('./routes/sauce')
+
+//DB connection
+mongoose.connect('mongodb+srv://root:cwJmnBNvydcfvfcf@cluster0.akxpe.mongodb.net/DB-P6?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussi !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
 
-app.use((req, res) => { // For each req
-    res.json({ messge: "request received" }); // send back json obj
+/**
+ * middlewares
+ */
+//autoriser le cors
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type', 'Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
+//Parse body request
+app.use(bodyParser.json());
 
+/**
+ * Routes
+ */
+
+app.use('/api/sauces', sauceRoutes);
 module.exports = app;
