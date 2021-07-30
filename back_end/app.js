1504008ced3bf73dd78b489f3fe1cd66d355e999
ArnaudPB <arnaud.besson@gmail.com>
2021-07-30 17:11:45 +0200

    [UPD] upgraded cors security

--- back_end/app.js
+++ back_end/app.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_P
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.AUTHORIZED_ORIGIN);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type', 'Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
